5984ad79e776c8a27406d7cdc8214ed00247a463
Riley Waghorn <869riley@gmail.com>
2024-10-15 18:39:40 +0100

    Update packager.py

--- core/packager.py
+++ core/packager.py
@@ -1,3 +1,25 @@
-print("Package Path:)
-path == input()
+import time
 
+while True:
+    print('Package File:')
+    package = input()
+
+    print("Is this the correct file?")
+    print(package)
+    verify = input("Y/N (Default: Y):").upper()
+
+    if verify == "Y" or verify == "":
+        break
+    else:
+        print("Please try again.")
+        
+autho = input("Would you like to verify this package? Unauthorised patches can harm your device. Y/N (Default: Y): \n").upper()
+if verify == "Y" or verify == "":
+        print("Verifying...")
+        time.sleep(4)
+        
+print("Connect your device to the computer.")
+time.sleep(3)
+print("iPad 9th Generation")
+time.sleep(0.9)
+print("Please verify that "+package+" is the correct patch for your device.")
